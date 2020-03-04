f6b987d10556489b93e7a5f03e1e7eaa4bcaf47c
MOZGIII <mike-n@narod.ru>
2020-03-04 05:48:43 +0300

    Add here_now_single_channel test

--- pubnub-hyper/tests/presence.rs
+++ pubnub-hyper/tests/presence.rs
@@ -1,11 +1,30 @@
-use pubnub_hyper::core::data::{channel, request};
+use pubnub_hyper::core::data::{channel, presence, request, response, timetoken::Timetoken};
 use pubnub_hyper::runtime::tokio_global::TokioGlobal;
 use pubnub_hyper::transport::hyper::Hyper;
 use pubnub_hyper::Builder;
+use std::convert::TryInto;
+use std::fmt::Write;
 
 mod common;
 
 const SAMPLE_UUID: &'static str = "903145ee-7c15-4579-aa5d-38a900717512";
+
+/// Delay execution for the specified amount of milliseconds.
+async fn sleep(ms: u64) {
+    tokio::time::delay_for(std::time::Duration::from_millis(ms)).await
+}
+
+/// Generate a string of random numbers.
+fn random_hex_string() -> String {
+    use getrandom::getrandom;
+    let mut buf = [0u8; 4];
+    getrandom(&mut buf).expect("failed to getrandom");
+    let mut s = String::new();
+    for b in buf.iter() {
+        write!(s, "{:02X}", b).unwrap();
+    }
+    s
+}
 
 #[test]
 fn get_set_state() {
@@ -46,6 +65,95 @@ fn get_set_state() {
                 })
                 .await;
             assert_eq!(val.unwrap(), sample_state);
+        }
+    });
+}
+
+#[test]
+fn here_now_single_channel() {
+    common::init();
+    common::current_thread_block_on(async {
+        let transport = Hyper::new()
+            .agent("Rust-Agent-Test")
+            .publish_key("demo")
+            .subscribe_key("demo")
+            .build()
+            .unwrap();
+
+        let pubnub = Builder::with_components(transport, TokioGlobal).build();
+
+        let test_channel: channel::Name = format!("my-channel-{}", random_hex_string())
+            .try_into()
+            .unwrap();
+
+        {
+            let val = pubnub
+                .call(request::Subscribe {
+                    channels: vec![test_channel.clone()],
+                    channel_groups: vec![],
+                    timetoken: Timetoken::default(),
+                })
+                .await;
+            assert!(val.is_ok());
+        }
+
+        // Wait for PunNub network to react.
+        sleep(10000).await;
+
+        {
+            let val = pubnub
+                .call(request::HereNow::<presence::respond_with::OccupancyOnly> {
+                    channels: vec![test_channel.clone()],
+                    channel_groups: vec![],
+                    respond_with: std::marker::PhantomData,
+                })
+                .await
+                .unwrap();
+            assert_eq!(
+                val,
+                response::HereNow::<presence::respond_with::OccupancyOnly> { occupancy: 1 }
+            );
+        }
+
+        {
+            let val = pubnub
+                .call(
+                    request::HereNow::<presence::respond_with::OccupancyAndUUIDs> {
+                        channels: vec![test_channel.clone()],
+                        channel_groups: vec![],
+                        respond_with: std::marker::PhantomData,
+                    },
+                )
+                .await
+                .unwrap();
+            assert_eq!(
+                val,
+                response::HereNow::<presence::respond_with::OccupancyAndUUIDs> {
+                    occupancy: 1,
+                    occupants: vec![pubnub.transport().uuid().clone()]
+                }
+            );
+        }
+
+        {
+            let val = pubnub
+                .call(request::HereNow::<presence::respond_with::Full> {
+                    channels: vec![test_channel.clone()],
+                    channel_groups: vec![],
+                    respond_with: std::marker::PhantomData,
+                })
+                .await
+                .unwrap();
+            assert_eq!(
+                val,
+                response::HereNow::<presence::respond_with::Full> {
+                    occupancy: 1,
+                    occupants: vec![presence::ChannelOccupantFullDetails {
+                        uuid: pubnub.transport().uuid().clone(),
+                        state: json::Null,
+                    }]
+                }
+            );
         }
     });
 }
