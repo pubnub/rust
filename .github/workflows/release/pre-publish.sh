b5046b3d3c2eb82a45efdb58b704ee35856edf05
Serhii Mamontov <parfeon@me.com>
2023-03-08 14:38:57 +0100

    refactor: rename script
    Renaming script to run right before commit changes to change logs and versions.

--- .github/workflows/release/pre-publish.sh
+++ .github/workflows/release/pre-publish.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # TODO: check if it is even possible with our bot
+# Now it will be called right after change log and version bump will be done.
 
 # main README.md
 cargo readme -r crates/pubnub --no-title --no-indent-headings --no-license > README.md
