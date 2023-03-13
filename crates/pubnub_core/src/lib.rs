56450febe3c3975846d0ba7b44531e37e704dc79
Lukasz Klich <kleewho@users.noreply.github.com>
2023-03-13 16:08:34 +0100

    Initial transport definitions (#72)

---
+++ crates/pubnub_core/src/lib.rs
@@ -0,0 +1,4 @@
+pub use transport_request::TransportMethod;
+mod transport;
+mod transport_request;
+mod transport_response;
---
+++ crates/pubnub_core/src/transport_response.rs
@@ -0,0 +1,8 @@
+use std::collections::HashMap;
+
+#[derive(Clone, Eq, PartialEq, Debug, Default)]
+pub struct TransportResponse {
+    status: u16,
+    headers: HashMap<String, String>,
+    body: Option<Vec<u8>>,
+}
