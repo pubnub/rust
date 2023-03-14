46460dcaeca6cd7534d4e3b703db5be4508a2ee4
Mohit Tejani <mohit.tejani@pubnub.com>
2023-03-14 15:01:33 +0530

    parser does not need to be async

--- crates/pubnub_core/src/parser.rs
+++ crates/pubnub_core/src/parser.rs
@@ -1,6 +1,5 @@
-#[async_trait::async_trait]
 pub trait Parser<T> {
-    async fn decode(&self, input: String) -> Result<T>;
-    async fn encode(&self, input: T) -> Result<String>;
+    fn decode(&self, input: String) -> Result<T>;
+    fn encode(&self, input: T) -> Result<String>;
 }
 
--- pubnub_parser/src/json_parser.rs
+++ pubnub_parser/src/json_parser.rs
@@ -1,11 +1,11 @@
 use serde::{Serialize, Deserialize};
 
 impl JsonParser for Parser<T> {
-  async fn decode(&self, input: String) {
+  fn decode(&self, input: String) {
     // serde_json::from_str(input).unwrap();
   }
 
-  async fn encode(&self, input: T) {
+  fn encode(&self, input: T) {
     // serde_json::to_string(&input).unwrap();
   }
 }
