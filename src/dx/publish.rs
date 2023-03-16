d5bca4a56b4a953b707d7c777922e7d8938155de
Xavrax <matedahlke@gmail.com>
2023-03-16 16:54:29 +0100

    my bad

--- src/dx/publish.rs
+++ src/dx/publish.rs
@@ -49,61 +49,20 @@ where
     /// TODO: Add documentation
     channel: String,
     /// TODO: Add documentation
-    #[builder(setter(strip_option), default = "None")]
+    #[builder(setter(strip_option), default)]
     store: Option<bool>,
     /// TODO: Add documentation
     #[builder(default = "true")]
     replicate: bool,
     /// TODO: Add documentation
-    #[builder(setter(strip_option), default = "None")]
+    #[builder(setter(strip_option), default)]
     ttl: Option<u32>,
     /// TODO: Add documentation
-    #[builder(setter(strip_option), default = "false")]
+    #[builder(default = "false")]
     use_post: bool,
     /// TODO: Add documentation
-    #[builder(setter(strip_option), default = "None")]
-    meta: Option<HashMap<String, String>>,
-    /// TODO: Add documentation
-    #[builder(setter(strip_option), default = "None")]
-    space_id: Option<String>,
-    /// TODO: Add documentation
-    #[builder(setter(strip_option), default = "None")]
-    message_type: Option<String>,
-}
-
-fn bool_to_numeric(value: bool) -> String {
-    if value { "1" } else { "0" }.to_string()
-}
-
-fn prepare_publish_query_params<T>(
-    publish_struct: &PublishMessageViaChannel<T>,
-) -> HashMap<String, String>
-where
-    T: Transport,
-{
-    let mut query_params: HashMap<String, String> = HashMap::new();
-
-    if let Some(store) = publish_struct.store {
-        query_params.insert("store".to_string(), bool_to_numeric(store));
-    }
-
-    if let Some(ttl) = publish_struct.ttl {
-        query_params.insert("ttl".to_string(), ttl.to_string());
-    }
-
-    if !publish_struct.replicate {
-        query_params.insert("norep".to_string(), true.to_string());
-    }
-
-    if let Some(space_id) = publish_struct.space_id {
-        query_params.insert("space-id".to_string(), space_id);
-    }
-
-    if let Some(message_type) = &publish_struct.message_type {
-        query_params.insert("type".to_string(), message_type.clone());
-    }
-
-    query_params
+    #[builder(default = "HashMap::new()")]
+    meta: HashMap<String, String>,
 }
 
 impl<'pub_nub, T> PublishMessageViaChannelBuilder<'pub_nub, T>
@@ -119,13 +78,10 @@ where
         let pub_key = "";
         let sub_key = "";
 
-        let query_params = prepare_publish_query_params(&instance);
-
         let request = if instance.use_post {
             TransportRequest {
                 path: format!("publish/{sub_key}/{pub_key}/0/{}/0", instance.channel),
                 method: TransportMethod::Post,
-                query_parameters: query_params,
                 //body: self.message.unwrap(), TODO
                 ..Default::default()
             }
@@ -136,7 +92,6 @@ where
                     sub_key, pub_key, instance.channel, instance.message
                 ),
                 method: TransportMethod::Get,
-                query_parameters: query_params,
                 ..Default::default()
             }
         };
@@ -195,49 +150,6 @@ mod should {
             .publish_message("First message".into())
             .channel("Iguess".into())
             .replicate(true)
-            .execute()
-            .await;
-
-        assert!(dbg!(result).is_ok());
-    }
-
-    #[tokio::test]
-    async fn verify_all_query_parameters() {
-        #[derive(Default)]
-        struct MockTransport;
-
-        #[async_trait::async_trait]
-        impl Transport for MockTransport {
-            async fn send(
-                &self,
-                request: TransportRequest,
-            ) -> Result<TransportResponse, PubNubError> {
-                assert_eq!(
-                    HashMap::<String, String>::from([
-                        ("norep".into(), "true".into()),
-                        ("store".into(), "1".into()),
-                        ("space-id".into(), "space_id".into()),
-                        ("type".into(), "message_type".into()),
-                        ("ttl".into(), "50".into())
-                    ]),
-                    request.query_parameters
-                );
-                Ok(TransportResponse::default())
-            }
-        }
-
-        let client = PubNubClient {
-            transport: MockTransport::default(),
-        };
-
-        let result = client
-            .publish_message("message".into())
-            .channel("chan".into())
-            .replicate(false)
-            .ttl(50)
-            .store(true)
-            .space_id("space_id".into())
-            .message_type("message_type".into())
             .execute()
             .await;
 
