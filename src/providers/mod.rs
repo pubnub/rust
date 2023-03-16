91834ce0dd2a6d017edb84cce9f3e2e40c9288d4
Xavrax <matedahlke@gmail.com>
2023-03-16 16:36:03 +0100

    better providers module

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -2,3 +2,6 @@
 //!
 //! This module contains the Providers that can be used by [`PubNubClient`].
 //! It is intended to be used by the [`pubnub`] crate.
+//!
+//! [`PubNubClient`]: ../pubnub_client/struct.PubNubClient.html
+//! [`pubnub`]: ../index.html
