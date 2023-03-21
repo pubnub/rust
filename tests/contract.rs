4acc5a9908deaca6c7d401552f336a2696a4d2f8
Mohit Tejani <mohit.tejani@pubnub.com>
2023-03-21 13:26:14 +0530

    WIP: some more tests steps implementation

--- tests/contract.rs
+++ tests/contract.rs
@@ -5,7 +5,6 @@ use pubnub::transport::middleware::PubNubMiddleware;
 use pubnub::transport::TransportReqwest;
 use pubnub::PubNubError;
 use reqwest::Client;
-use serde::Deserialize;
 use std::collections::HashMap;
 
 #[derive(Debug, Default)]
@@ -103,6 +102,59 @@ async fn i_publish_dictionary_as_message_to_channel_as_post_body(
         .await;
 }
 
+#[when(regex = r"^I publish (.*) dictionary as message to (.*) channel$")]
+async fn i_publish_dictionary_as_message_to_channel(
+    world: &mut PubNubWorld,
+    dictionary_json: String,
+    channel: String,
+) {
+    let message_hash_map: HashMap<String, String> =
+        serde_json::from_str(dictionary_json.trim_matches('\'')).unwrap();
+    world.last_result = world
+        .get_pub_nub()
+        .publish_message(message_hash_map)
+        .channel(channel)
+        .execute()
+        .await;
+}
+
+#[when(regex = r"^I publish (.*) array as message to (.*) channel$")]
+async fn i_publish_array_as_message_to_channel(
+    world: &mut PubNubWorld,
+    array_str: String,
+    channel: String,
+) {
+    let message_array: [String; 2] = serde_json::from_str(array_str.trim_matches('\'')).unwrap();
+    world.last_result = world
+        .get_pub_nub()
+        .publish_message(message_array)
+        .channel(channel)
+        .execute()
+        .await;
+}
+
+#[when(regex = r"^I publish (.*) string as message to (.*) channel with (.*) set to (.*)$")]
+async fn i_publish_message_to_channel_with_meta(
+    world: &mut PubNubWorld,
+    message: String,
+    channel: String,
+    param: String,
+    json_str: String
+) {
+    let meta_map: HashMap<String, String> =
+        serde_json::from_str(json_str.trim_matches('\'')).unwrap();
+            print!("sx");
+        if param.trim_matches('\'').eq("meta") {
+            world.last_result = world
+                .get_pub_nub()
+                .publish_message(message)
+                .channel(channel)
+                .meta(meta_map)
+                .execute()
+                .await;
+        }
+}
+
 #[when(expr = "I publish '{int}' number as message to '{word}' channel")]
 async fn i_publish_number_as_message_to_channel(
     world: &mut PubNubWorld,
@@ -140,4 +192,3 @@ async fn main() {
         .run_and_exit("tests/features/publish")
         .await;
 }
-//no calls to tomato
