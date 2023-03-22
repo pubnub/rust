d84948d2a3eca4e49a8c2d0ad6c0eaf7a7bbabd8
Mohit Tejani <60129002+mohitpubnub@users.noreply.github.com>
2023-03-22 19:41:59 +0530

    phoenix-dev/publish-test (#91)
    * WIP
    
    * Dictionary not working
    
    * WIP: fix skipped step
    
    * * Dictionary Workin now *fix: serialisation of dictionary
    
    * link not required
    
    * WIP: some more tests steps implementation
    
    * WIP: cleanup. without update
    
    * WIP: transport reqwest
    
    * add error for not 200 status codfe
    
    * fix: expressions
    
    * merge phoenix and required additional changes
    * making config fields pub so that user can set it
    
    * encoding for publish content
    
    * added docs things as TODO for clippy
    
    * Update tests/contract.rs
    
    Co-authored-by: Mateusz Dahlke <39696234+Xavrax@users.noreply.github.com>
    
    * rollback visibility related changes. Builder at tests
    
    * pubnub_client visibility of fields
    
    * clippy
    
    * typo
    
    ---------
    
    Co-authored-by: Lukasz Klich <lukasz.klich@pubnub.com>
    Co-authored-by: Xavrax <matedahlke@gmail.com>
    Co-authored-by: Mateusz Dahlke <39696234+Xavrax@users.noreply.github.com>

--- tests/contract.rs
+++ tests/contract.rs
@@ -1,34 +1,53 @@
 use cucumber::{given, then, when, World};
+use pubnub::dx::publish::PublishResult;
+use pubnub::dx::PubNubClient;
+use pubnub::transport::middleware::PubNubMiddleware;
+use pubnub::transport::TransportReqwest;
+use pubnub::{Keyset, PubNubClientBuilder, PubNubError};
+use std::collections::HashMap;
 
-#[derive(Debug, Default)]
-struct Keyset {
-    pub subkey: String,
-    pub pubkey: String,
+#[derive(Debug, World)]
+pub struct PubNubWorld {
+    keyset: pubnub::Keyset<String>,
+    last_result: Result<PublishResult, PubNubError>,
 }
 
-#[derive(Debug, Default, World)]
-pub struct PubnubWorld {
-    keyset: Keyset,
-    last_result: String,
+impl Default for PubNubWorld {
+    fn default() -> Self {
+        PubNubWorld {
+            keyset: Keyset::<String> {
+                subscribe_key: "demo".to_owned(),
+                publish_key: Some("demo".to_string()),
+                secret_key: Some("demo".to_string()),
+            },
+            last_result: Err(PubNubError::TransportError("This is default value".into())),
+        }
+    }
+}
+
+impl PubNubWorld {
+    fn get_pub_nub(
+        &self,
+        keyset: Keyset<String>,
+    ) -> PubNubClient<PubNubMiddleware<TransportReqwest>> {
+        let transport = {
+            let mut transport = TransportReqwest::default();
+            transport.hostname = "http://localhost:8090/".into();
+            transport
+        };
+        PubNubClientBuilder::with_reqwest_transport()
+            .with_transport(transport)
+            .with_keyset(keyset)
+            .with_user_id("test")
+            .build()
+            .unwrap()
+    }
 }
 
 #[given("the demo keyset")]
-fn set_keyset(world: &mut PubnubWorld) {
-    world.keyset.pubkey = "demo".to_string();
-    world.keyset.subkey = "demo".to_string();
-}
-
-#[given("a message")]
-fn message_defined(_world: &mut PubnubWorld) {}
-
-#[when("I publish a message")]
-fn pubnub_publish(world: &mut PubnubWorld) {
-    world.last_result = String::from("1234567890");
-}
-
-#[then(expr = "I get a timetoken {word}")]
-fn check_timetoken(world: &mut PubnubWorld, timetoken: String) {
-    assert_eq!(world.last_result, timetoken);
+fn set_keyset(world: &mut PubNubWorld) {
+    world.keyset.publish_key = Some("demo".to_string());
+    world.keyset.subscribe_key = "demo".to_string();
 }
 
 async fn init_server(script: String) -> Result<String, Box<dyn std::error::Error>> {
@@ -38,9 +57,133 @@ async fn init_server(script: String) -> Result<String, Box<dyn std::error::Error
     Ok(body)
 }
 
+#[when(expr = "I publish '{word}' string as message to '{word}' channel")]
+async fn i_publish_string_as_message_to_channel(
+    world: &mut PubNubWorld,
+    message: String,
+    channel: String,
+) {
+    world.last_result = world
+        .get_pub_nub(world.keyset.to_owned())
+        .publish_message(message)
+        .channel(channel)
+        .execute()
+        .await;
+}
+
+#[then("I receive successful response")]
+fn i_receive_successful_response(world: &mut PubNubWorld) {
+    assert!(world.last_result.is_ok())
+}
+
+#[when(expr = "I publish {string} dictionary as message to '{word}' channel with compression")]
+fn i_publish_dictionary_as_message_to_channel_with_compression(
+    _world: &mut PubNubWorld,
+    _dictionary_json: String,
+    _channel: String,
+) {
+}
+
+#[when(regex = r"^I publish '(.*)' dictionary as message to '(.*)' channel as POST body$")]
+async fn i_publish_dictionary_as_message_to_channel_as_post_body(
+    world: &mut PubNubWorld,
+    dictionary_json: String,
+    channel: String,
+) {
+    let message_hash_map: HashMap<String, String> =
+        serde_json::from_str(dictionary_json.as_str()).unwrap();
+    world.last_result = world
+        .get_pub_nub(world.keyset.to_owned())
+        .publish_message(message_hash_map)
+        .channel(channel)
+        .use_post(true)
+        .execute()
+        .await;
+}
+
+#[when(regex = r"^I publish '(.*)' dictionary as message to '(.*)' channel$")]
+async fn i_publish_dictionary_as_message_to_channel(
+    world: &mut PubNubWorld,
+    dictionary_json: String,
+    channel: String,
+) {
+    let message_hash_map: HashMap<String, String> =
+        serde_json::from_str(dictionary_json.as_str()).unwrap();
+    world.last_result = world
+        .get_pub_nub(world.keyset.to_owned())
+        .publish_message(message_hash_map)
+        .channel(channel)
+        .execute()
+        .await;
+}
+
+#[when(regex = r"^I publish '(.*)' array as message to '(.*)' channel$")]
+async fn i_publish_array_as_message_to_channel(
+    world: &mut PubNubWorld,
+    array_str: String,
+    channel: String,
+) {
+    let message_array: [String; 2] = serde_json::from_str(array_str.as_str()).unwrap();
+    world.last_result = world
+        .get_pub_nub(world.keyset.to_owned())
+        .publish_message(message_array)
+        .channel(channel)
+        .execute()
+        .await;
+}
+
+#[when(regex = r"^I publish '(.*)' string as message to '(.*)' channel with '(.*)' set to '(.*)'$")]
+async fn i_publish_message_to_channel_with_meta(
+    world: &mut PubNubWorld,
+    message: String,
+    channel: String,
+    param: String,
+    param_value: String,
+) {
+    match param.as_str() {
+        "meta" => {
+            let meta_map: HashMap<String, String> =
+                serde_json::from_str(param_value.as_str()).unwrap();
+            world.last_result = world
+                .get_pub_nub(world.keyset.to_owned())
+                .publish_message(message)
+                .channel(channel)
+                .meta(meta_map)
+                .execute()
+                .await;
+        }
+        "store" => {
+            let store: bool = param_value != "0";
+            world.last_result = world
+                .get_pub_nub(world.keyset.to_owned())
+                .publish_message(message)
+                .channel(channel)
+                .store(store)
+                .execute()
+                .await;
+        }
+        _ => { /* do nothing */ }
+    }
+}
+
+#[when(expr = "I publish '{int}' number as message to '{word}' channel")]
+async fn i_publish_number_as_message_to_channel(
+    world: &mut PubNubWorld,
+    number: i32,
+    channel: String,
+) {
+    world.last_result = world
+        .get_pub_nub(world.keyset.to_owned())
+        .publish_message(number)
+        .channel(channel)
+        .execute()
+        .await;
+}
+
 #[tokio::main]
 async fn main() {
-    PubnubWorld::cucumber()
+    env_logger::builder().try_init().unwrap();
+    PubNubWorld::cucumber()
         .before(|_feature, _rule, scenario, _world| {
             futures::FutureExt::boxed(async move {
                 if scenario.tags.iter().any(|t| t.starts_with("contract=")) {
