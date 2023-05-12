35dfa20aba04588f287c218f1127943d2f898c0b
Xavrax <matedahlke@gmail.com>
2023-05-12 16:03:22 +0200

    main pam

--- examples/pam.rs
+++ examples/pam.rs
@@ -1,6 +1,5 @@
-use pubnub::core::PubNubError;
-use pubnub::{access::*, Keyset, PubNubClientBuilder};
-use std::env;
+use pubnub::{access::*, core::PubNubError, parse_token, Keyset, PubNubClientBuilder, Token};
+use std::{collections::HashMap, env};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -28,11 +27,73 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .join()
             .read()
             .write()])
-        .meta([("owner-role".into(), "admin".into())].into())
+        .meta(HashMap::from([("owner-role".into(), "admin".into())]))
         .execute()
         .await?;
 
-    println!("Access token: {}", grant_result.token.clone());
+    println!(
+        "Access token - call with metadata: {}",
+        grant_result.token.clone()
+    );
+
+    // Grant an authorized client different levels of access to various resources in a single call
+    let grant_result_various_resources = client
+        .grant_token(10)
+        .authorized_user_id("my-authorized-user_id")
+        .resources(&[
+            permissions::channel("channel-a").read(),
+            permissions::channel_group("channel-group-b").read(),
+            permissions::user_id("uuid-c").get(),
+            permissions::channel("channel-b").read().write(),
+            permissions::channel("channel-c").read().write(),
+            permissions::channel("channel-d").read().write(),
+            permissions::user_id("uuid-d").get().update(),
+        ])
+        .execute()
+        .await?;
+
+    println!(
+        "Access token - call with various resources: {}",
+        grant_result_various_resources.token.clone()
+    );
+
+    // Grant an authorized client multiple channels using RegEx
+    let grant_result_multiple_channels_regex = client
+        .grant_token(10)
+        .authorized_user_id("my-authorized-user_id")
+        .patterns(&[permissions::channel("^channel-[A-Za-z0-9]$").read()])
+        .execute()
+        .await?;
+
+    println!(
+        "Access token - call with multiple channels using RegEx: {}",
+        grant_result_multiple_channels_regex.token.clone()
+    );
+
+    // Grant an authorized client different levels of access to various resources and read access to channels using RegEx
+
+    let grant_result_different_levels_resources_patterns_regex = client
+        .grant_token(10)
+        .authorized_user_id("my-authorized-user_id")
+        .resources(&[
+            permissions::channel("channel-a").read(),
+            permissions::channel_group("channel-group-b").read(),
+            permissions::user_id("uuid-c").get(),
+            permissions::channel("channel-b").read().write(),
+            permissions::channel("channel-c").read().write(),
+            permissions::channel("channel-d").read().write(),
+            permissions::user_id("uuid-d").get().update(),
+        ])
+        .patterns(&[permissions::channel("^channel-[A-Za-z0-9]$").read()])
+        .execute()
+        .await?;
+
+    println!(
+        "Access token - call with different resources and patterns: {}",
+        grant_result_different_levels_resources_patterns_regex
+            .token
+            .clone()
+    );
 
     // Revoke token permissions.
     client
@@ -51,6 +112,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             message, status
         );
     };
+
+    match parse_token(grant_result_various_resources.token.clone().as_str()) {
+        Ok(Token::V2(token)) => println!("Token information: {token:#?}"),
+        Err(err) => println!("Token parse error: {err:#?}"),
+    }
 
     Ok(())
 }
