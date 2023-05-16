601add6db7e99174c0b6752d499a98c514f24176
Serhii Mamontov <parfeon@me.com>
2023-05-16 16:52:53 +0300

    refactor(github-action): add `cargo-readme` install call

--- .github/workflows/release/pre-publish.sh
+++ .github/workflows/release/pre-publish.sh
@@ -3,6 +3,8 @@
 # TODO: check if it is even possible with our bot
 # Now it will be called right after change log and version bump will be done.
 
+cargo install cargo-readme
+
 # main README.md
 cargo readme --no-title --no-indent-headings --no-license > README.md
 
