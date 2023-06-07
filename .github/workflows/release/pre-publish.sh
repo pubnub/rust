9e4e10a203cf2ca086c46fd534def5384a88fb7f
Serhii Mamontov <parfeon@me.com>
2023-06-07 09:42:08 +0300

    Use custom cargo action for release (#152)
    build(cargo): use custom cargo action for release

--- .github/workflows/release/pre-publish.sh
+++ .github/workflows/release/pre-publish.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# TODO: check if it is even possible with our bot
-# Now it will be called right after change log and version bump will be done.
-
 cargo install cargo-readme
 
 # main README.md
