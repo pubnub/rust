75c8372c574aeb742f7591a4505c932eba770b98
Xavrax <matedahlke@gmail.com>
2023-07-07 12:41:18 +0200

    cancel unit test

--- src/dx/subscribe/cancel.rs
+++ src/dx/subscribe/cancel.rs
@@ -32,3 +32,22 @@ impl CancelationTask {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[tokio::test]
+    async fn wait_for_cancel() {
+        let (cancel_tx, cancel_rx) = async_channel::bounded(2);
+
+        let cancel_task = CancelationTask::new(cancel_rx, "id".into());
+
+        tokio::spawn(async move {
+            tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+            cancel_tx.send("id".into()).await.unwrap();
+        });
+
+        cancel_task.wait_for_cancel().await.unwrap();
+    }
+}
