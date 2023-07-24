ec1a63cbf5091afda40a1d27e390d34580ba3645
Xavrax <matedahlke@gmail.com>
2023-07-24 11:56:35 +0200

    tweaking subscribe example

--- examples/subscribe.rs
+++ examples/subscribe.rs
@@ -1,25 +1,23 @@
-#[cfg(feature = "std")]
 use futures::StreamExt;
-#[cfg(feature = "std")]
 use pubnub::dx::subscribe::{SubscribeStreamEvent, Update};
-#[cfg(feature = "std")]
 use pubnub::{Keyset, PubNubClientBuilder};
 use serde::Deserialize;
-#[cfg(feature = "std")]
 use std::env;
 
-#[derive(Deserialize)]
-#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
 struct Message {
+    // Allowing dead code because we don't use these fields
+    // in this example.
+    #[allow(dead_code)]
     url: String,
+    #[allow(dead_code)]
     description: String,
 }
 
 #[tokio::main]
-#[cfg(feature = "std")]
 async fn main() -> Result<(), Box<dyn snafu::Error>> {
-    let publish_key = env::var("SDK_PUB_KEY")?;
-    let subscribe_key = env::var("SDK_SUB_KEY")?;
+    let publish_key = "demo"; //env::var("SDK_PUB_KEY")?;
+    let subscribe_key = "demo"; //env::var("SDK_SUB_KEY")?;
 
     let client = PubNubClientBuilder::with_reqwest_transport()
         .with_keyset(Keyset {
@@ -45,7 +43,13 @@ async fn main() -> Result<(), Box<dyn snafu::Error>> {
                     println!("update: {:?}", update);
                     match update {
                         Update::Message(message) | Update::Signal(message) => {
-                            println!("message: {:?}", String::from_utf8(message.data))
+                            // Deserialize the message payload as you wish
+                            match serde_json::from_slice::<Message>(&message.data) {
+                                Ok(message) => println!("defined message: {:?}", message),
+                                Err(_) => {
+                                    println!("other message: {:?}", String::from_utf8(message.data))
+                                }
+                            }
                         }
                         Update::Presence(presence) => {
                             println!("presence: {:?}", presence)
@@ -66,11 +70,5 @@ async fn main() -> Result<(), Box<dyn snafu::Error>> {
         })
         .await;
 
-    Ok(())
-}
-
-#[tokio::main]
-#[cfg(not(feature = "std"))]
-async fn main() -> Result<(), Box<dyn snafu::Error>> {
     Ok(())
 }
