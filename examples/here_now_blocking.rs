bc662ef2014bccf2914101f193089bea3b665fbf
Xavrax <matedahlke@gmail.com>
2023-08-29 23:17:17 +0200

    proper examples

--- examples/here_now_blocking.rs
+++ examples/here_now_blocking.rs
@@ -1,12 +1,11 @@
 use pubnub::{Keyset, PubNubClientBuilder};
 use std::env;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn snafu::Error>> {
+fn main() -> Result<(), Box<dyn snafu::Error>> {
     let publish_key = env::var("SDK_PUB_KEY")?;
     let subscribe_key = env::var("SDK_SUB_KEY")?;
 
-    let client = PubNubClientBuilder::with_reqwest_transport()
+    let client = PubNubClientBuilder::with_reqwest_blocking_transport()
         .with_keyset(Keyset {
             subscribe_key,
             publish_key: Some(publish_key),
@@ -22,8 +21,7 @@ async fn main() -> Result<(), Box<dyn snafu::Error>> {
         .channels(["my_channel".into(), "other_channel".into()].to_vec())
         .include_state(true)
         .include_user_id(true)
-        .execute()
-        .await?;
+        .execute_blocking()?;
 
     println!("All channels data: {:?}\n", channels_now);
 
